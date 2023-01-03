3c4406a828c6d2b0bd1bf504cc3b30b24557f8ea
Paavo Rotstén <paavo.rotsten@aalto.fi>
2023-01-03 14:20:46 +0200

    Added more camera control

--- src/main/java/entity/CameraController.java
+++ src/main/java/entity/CameraController.java
@@ -9,6 +9,7 @@ import org.lwjgl.glfw.GLFW;
 public class CameraController extends Component {
 
     private static final float SPEED = 0.01f;
+    private static final float ROT_SPEED = 0.001f;
 
     @Override public void start() {}
     @Override public void stop() {}
@@ -16,17 +17,24 @@ public class CameraController extends Component {
         var transform = EntityManager.getComponent(camera, TransformationComponent.class);
 
         var move = SPEED * Timer.getFrametimeMillis();
+        var rot = ROT_SPEED * Timer.getFrametimeMillis();
 
-        Vector3f delta = new Vector3f(0, 0, 0);
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_W)) delta.z += move;
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_S)) delta.z -= move;
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_A)) delta.x += move;
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_D)) delta.x -= move;
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_SPACE))      delta.y += move;
-        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT)) delta.y -= move;
+        Vector3f deltaPos = new Vector3f(0, 0, 0);
+        Vector3f deltaRot = new Vector3f(0, 0, 0);
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_W)) deltaPos.z += move;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_S)) deltaPos.z -= move;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_A)) deltaPos.x += move;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_D)) deltaPos.x -= move;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_SPACE))      deltaPos.y += move;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT_SHIFT)) deltaPos.y -= move;
 
-        var prev = transform.getPosition();
-        transform.setPosition(delta.add(prev)); // -1 0 0
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_LEFT))  deltaRot.y -= rot;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_RIGHT)) deltaRot.y += rot;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_UP))    deltaRot.x -= rot;
+        if (Keyboard.isKeyDown(GLFW.GLFW_KEY_DOWN))  deltaRot.x += rot;
+
+        transform.setPosition(deltaPos.add(transform.getPosition()));
+        transform.setRotation(deltaRot.add(transform.getRotation()));
     }
 
 }
